3bb1fd2b28bd4fa95134b6f8c2208392c6820682
Seraf <milerbrane@gmail.com>
2020-04-09 17:42:07 +0200

    Initial commit

---
+++ client/component/Card/CardExchanges.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+import Card from './Card';
+
+const CardExchanges = () => (
+  //@todo move this to config to avoid conflicts
+  <Card title="Exchanges">
+    <a href="https://txbit.io/Trade/BWK/BTC" target="_blank" rel="nofollow noopener">Txbit.io</a><br />
+    <a href="https://graviex.net/markets/bwkbtc/" target="_blank" rel="nofollow noopener">Graviex</a><br />
+    <a href="https://bastionex.com/" target="_blank" rel="nofollow noopener">Bastion</a><br />
+    <a href="https://blocknet.co/" target="_blank" rel="nofollow noopener">Blocknet</a><br />
+  </Card>
+);
+
+export default CardExchanges;
---
+++ config.server.template.js
@@ -0,0 +1,36 @@
+/**
+ * Keep all your API & secrets here. DO NOT IMPORT THIS FILE IN /client folder
+ */
+const secretsConfig = {
+  db: {
+    host: 'localhost',
+    port: '27017',
+    name: 'blockex',
+    user: 'someuser',
+    pass: 'password'
+  },
+  rpc: {
+    host: 'localhost',
+    port: '52541',
+    user: 'somerpc',
+    pass: 'someverysafepassword',
+    timeout: 8000, // 8 seconds
+  },
+  social: {
+    reddit: {
+      /**
+       * 
+       * You will need to register your app on reddit: https://www.reddit.com/prefs/apps as "web app"
+       * You can generate these credentials via web interface: https://not-an-aardvark.github.io/reddit-oauth-helper/ 
+       * 
+       * You will need [read] "Access posts and comments through my account" scope ONLY. Do not check anything else for privacy & security reasons.
+       * 
+       **/
+      clientId: 'put your client id here', // This is from https://www.reddit.com/prefs/apps (the little numbers under 'web app' title next to question mark logo)
+      clientSecret: 'put your client secret here', // This is from https://www.reddit.com/prefs/apps (under secret label)
+      refreshToken: 'put your refresh token here' // This is on bottom of https://not-an-aardvark.github.io/reddit-oauth-helper/ (after you are redirected back)
+    }
+  }
+}
+
+module.exports = { secretsConfig }; // This is returned as an object on purpose so you have to be explicit at stating that you are accessing a secrets config
